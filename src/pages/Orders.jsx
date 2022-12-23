3f81bdb957d1ed84b58a5dd272c63992c0689f99
Pranay Kothari <pranay.kothari109@gmail.com>
2022-12-23 12:22:02 +0530

    tooltip added

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { Box, Container, Paper, Stack, Typography } from "@mui/material";
+import { Box, Container, Paper, Stack, Tooltip, Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { useContext } from "react";
 import UserContext from "../contexts/UserContext";
@@ -19,28 +19,43 @@ function Orders() {
   }, []);
 
   return (
-    <Container >
-      <Typography variant="h3" sx={{ pt: 2, pb: 5, textAlign: "center" }}>Orders</Typography>
+    <Container>
+      <Typography variant="h3" sx={{ pt: 2, pb: 5, textAlign: "center" }}>
+        Orders
+      </Typography>
       <Box>
-      {orders.map((s) => (
-        <Paper key={s._id} sx={{ mb: 3, borderRadius: 2, overflow: "hidden" }}>
-          <Stack direction="row" spacing={3} p={2}  style={{ backgroundColor: '#d3d3d3'}} >
-            <Typography>Order Id: {s.order_id}</Typography>
-            <Typography>Total: ₹{s.order_total}</Typography>
-            <Typography>Ship To: {s.customer_name}</Typography>
-          </Stack>
-          <Box p={2}>
-          {
-            s.products.map(product => (
-              <Box>
-              {/* <img src={product}/> */}
-                  {product.product_name}
-              </Box>
-            ))
-          }
-          </Box>
-        </Paper>
-      ))}
+        {orders.map((s) => (
+          <Paper
+            key={s._id}
+            sx={{ mb: 3, borderRadius: 2, overflow: "hidden" }}
+          >
+            <Stack
+              direction="row"
+              spacing={3}
+              p={2}
+              style={{ backgroundColor: "#d3d3d3" }}
+            >
+              <Typography>Order Id: {s.order_id}</Typography>
+              <Typography>Total: ₹{s.order_total}</Typography>
+            
+              <Tooltip title={s.shipping_address.address+", "+s.shipping_address.pincode} arrow={true}>
+                {/* <IconButton>
+                  <DeleteIcon />
+                </IconButton> */}
+              <Typography>Ship To: {s.shipping_address.name}</Typography>
+              </Tooltip>
+            </Stack>
+            <Stack p={2} spacing={1}>
+              {s.products.map((product) => (
+                <Stack direction="row" spacing={5} key={product._id}>
+                  <Box>{product.product_name}</Box>
+                  <Box>{product.quantity}</Box>
+                  <Box>₹{product.price}</Box>
+                </Stack>
+              ))}
+            </Stack>
+          </Paper>
+        ))}
       </Box>
     </Container>
   );
